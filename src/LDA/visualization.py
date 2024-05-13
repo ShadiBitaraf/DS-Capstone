8c8dc26032d3e9ac2aa0832aafb83e514f202d92
roseabsin <rose.absin16@gmail.com>
2024-05-13 02:47:34 -0700

    Added word cloud, but might need to edit later to take in parameter for df instead of a list of lists

--- src/LDA/visualization.py
+++ src/LDA/visualization.py
@@ -4,3 +4,26 @@ This file will handle visualizing the LDA topics and other relevant visualizatio
 Visualize the distribution of assigned topics.
 Generate word clouds for each topic.
 '''
+
+
+# Use WordCloud library to create visualizations
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+def create_wordcloud(topic_words):
+    fig = plt.figure(figsize=(80, 32))
+    for i, top_words_topic in enumerate(topic_words):  # Iterate through each list of top words for a topic
+        ax = fig.add_subplot(4, 3, i + 1)
+        text = ' '.join(top_words_topic)  # Join the top words into a single string
+        wc = WordCloud(width=1000,
+                    height=1000,
+                    random_state=1,
+                    background_color='gray',  # Changed background color for visibility
+                    colormap='Set2',  # You can adjust the colormap if needed
+                    collocations=False).generate(text)
+        ax.imshow(wc)
+        ax.set_title(f'Topic {i}')
+        ax.axis("off")
+    
+    plt.show()
